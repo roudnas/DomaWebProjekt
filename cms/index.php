7f6c8afc3857be162925702f364d4e507a9e0179
roudnas <dominikroudny@seznam.cz>
2019-01-09 19:19:33 +0100

    front-end changes in cms

--- cms/index.php
+++ cms/index.php
@@ -9,9 +9,9 @@ if (!isset($_SESSION['userId'])) {
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
      crossorigin="anonymous">
+     <link rel="stylesheet" href="public/css/style.css">
     <link rel="icon" href="public/images/newLogoJustR.png">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
      crossorigin="anonymous"></script>
