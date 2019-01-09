84b1ae400b6ce79ac409bd2a6e91396a4db8cd50
roudnas <dominikroudny@seznam.cz>
2019-01-09 20:00:11 +0100

    Admin page v2.0

--- cms/index.php
+++ cms/index.php
@@ -24,8 +24,8 @@ if (!isset($_SESSION['userId'])) {
   <body data-spy="scroll" data-target=".nav" data-offset="0">
     <article class="menu">
       <nav class="nav nav-pills flex-column flex-sm-column">
-    <a class="navbar-brand my-3" href="#sekce1"><img src="public/images/DomaSoftware.svg" alt="" width="200px" height="140px"></a>
-    <hr class="my-3">
+    <a class="navbar-brand my-3" href="#sekce1"><div class="brandimg"></div> </a>
+    <hr class="my-3" style="background-color: rgba(255,255,255,0.15);">
     <a class="text-sm-center nav-link active" href="#sekce1">Správa obsahu</a>
     <a class="text-sm-center nav-link" href="#sekce2">Moderátoři</a>
     <a class="text-sm-center nav-link" href="#sekce3">Statistiky</a>
