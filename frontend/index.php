71683b8a31056535e2bf38603997ff14c5bd4417
MartmatiX <martin.malir@gmail.com>
2019-01-10 19:40:23 +0100

    jen zakladni rozvrzeni, jeste to cely predelam

--- frontend/index.php
+++ frontend/index.php
@@ -2,9 +2,9 @@
 <html lang="cs" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
      crossorigin="anonymous">
-     <link rel="stylesheet" href="public/css/style.css">
     <link rel="icon" href="public/images/newLogoJustR.png">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
      crossorigin="anonymous"></script>
@@ -17,7 +17,7 @@
   <body>
 
 <header class="menu">
-  <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top" id="navigator">
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark" id="navigator">
     <div class="container-fluid">
 
       <a class="navbar-brand" href="#Home"><img src="public/images/DomaSoftware.svg" alt="" width="130px" height="80px"></a>
@@ -38,6 +38,58 @@
   </nav>
 </header>
 
+<article class="py-3">
+  <header class="text-center container">
+    <div class="row justify-content-center">
+      <div class="col-11 col-sm-10 col-md-8">
+        <h3>O nás</h3>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </header>
+</article>
+
+<p id="line"></p>
+
+<article class="py-3">
+  <header class="text-center container">
+    <div class="row justify-content-center">
+      <div class="col-11 col-sm-10 col-md-8">
+        <h3>Co nabízíme</h3>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </header>
+</article>
+
+<p id="line"></p>
+
+<article class="py-3">
+  <header class="text-center container">
+    <div class="row justify-content-center">
+      <div class="col-11 col-sm-10 col-md-8">
+        <h3>DevBlog</h3>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </header>
+</article>
+
+
+<p id="line"></p>
+
+<article class="py-3">
+  <div class="container col-sm">
+    <div class="text-center container">
+      <h3>Kontakty</h3>
+    </div>
+
+    <div class="container col-sm">
+
+    </div>
+
+  </div>
+</article>
 
 
   </body>
