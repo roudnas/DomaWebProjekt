0e0fd82f4b57e3dddf868f77cfc550d2db0d4be6
roudnas <dominikroudny@seznam.cz>
2019-01-10 23:20:06 +0100

    Responsive changes

--- cms/index.php
+++ cms/index.php
@@ -39,7 +39,7 @@ if (!isset($_SESSION['userId'])) {
 		</nav>
 
     <article class="menu">
-      <nav class="sidenav nav nav-tabs flex-column flex-sm-column" id="sidenav">
+      <nav class="sidenav nav nav-tabs flex-column flex-sm-colum" id="sidenav">
         <hr class="my-3" style="background-color: rgba(255,255,255,0.5);">
     <a class="sidenav-link text-sm-center nav-link active" href="index.php"><img class="my-2 mx-2" src="https://img.icons8.com/material/24/000000/document.png">Správa obsahu</a>
     <a class="sidenav-link text-sm-center nav-link" href="moderatori.php"><img class="my-2 mx-2" src="https://img.icons8.com/material/24/000000/user-group-man-man.png">Moderátoři</a>
