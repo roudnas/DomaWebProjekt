f7f276cea8fa605d0f95a4fcee37fb45e6ae3323
roudnas <dominikroudny@seznam.cz>
2019-01-15 19:15:25 +0100

    meta update

--- frontend/coNabizime.php
+++ frontend/coNabizime.php
@@ -2,6 +2,8 @@
 <html lang="cs" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
      crossorigin="anonymous">
