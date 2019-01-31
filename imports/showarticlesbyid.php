fa302ca150c45d1b887fc5c141469ed083c99aa4
roudnas <dominikroudny@seznam.cz>
2019-01-31 12:02:18 +0100

    Text editor implement

--- imports/showarticlesbyid.php
+++ imports/showarticlesbyid.php
@@ -28,7 +28,7 @@ if (isset($getID) && is_numeric($getID)) {
             <h6 class='text-muted' style='overflow-y:hidden;'>$parsedDate</h6>
             <hr>
             <div class='p-2 mx-auto my-3' style='width:80%;'>
-              $showText
+              <div class='textDiv'>$showText</div>
             </div><section class='row justify-content-center'>";
              while($row1 = mysqli_fetch_assoc($result1)) {
                $imgName = $row1['name'];
